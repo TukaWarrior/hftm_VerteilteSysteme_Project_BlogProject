c72d6f2fff7ec9a31139818a08222230980b74c3
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-04-30 00:06:04 +0200

    Thinkering around, testing things.

--- src/main/java/ch/hftm/blogproject/GreetingResource.java
+++ src/main/java/ch/hftm/blogproject/GreetingResource.java
@@ -1,5 +1,11 @@
 package ch.hftm.blogproject;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +14,30 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+
+    private static int timesVisited = 0;
+    
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Willkommen auf meinem Blog!";
+        String randomFact = readRandomFactFile("src/main/resources/randomFacts.txt");
+        timesVisited++;
+        return "Willkommen auf meinem Blog! \n" + 
+        "Check out this: http://localhost:8080/duck\n " + 
+        "This site has been reloaded " + timesVisited + " times.\n" +
+        "\n" + 
+        "Random Fact I copied from the internet:\n" + randomFact;
+    }
+
+    private String readRandomFactFile(String filePath) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            Random random = new Random();
+            return lines.get(random.nextInt(lines.size()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Something failed. A team of highly skilled monkeys is working on the problem.";
+        }
     }
 }
