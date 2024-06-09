2be359ea25a6707dba540c07c61e3c9655861453
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-06-09 14:25:25 +0200

    Update Comment.java, added description

--- src/main/java/ch/hftm/blogproject/entity/Comment.java
+++ src/main/java/ch/hftm/blogproject/entity/Comment.java
@@ -1,3 +1,4 @@
+// This class is currently unused. It serves as a WIP idea t oimplement comments under a blog post.
 package ch.hftm.blogproject.entity;
 
 import jakarta.persistence.GeneratedValue;
