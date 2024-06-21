4a87e030afe8935f4d225f5c4ebafdf3150f0f4f
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-06-21 21:06:58 +0200

    Updated validation in NewBlogDTO to check for Blank titles and content

--- src/main/java/ch/hftm/blogproject/boundary/dto/NewBlogDTO.java
+++ src/main/java/ch/hftm/blogproject/boundary/dto/NewBlogDTO.java
@@ -1,10 +1,12 @@
 package ch.hftm.blogproject.boundary.dto;
 
 import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import ch.hftm.blogproject.entity.Blog;
 
-public record NewBlogDTO(@NotNull @Length(min = 5) String title, String content) {
+public record NewBlogDTO(@NotNull @NotBlank @Length(min = 5) String title, String content) {
     public Blog toBlog() {
         return new Blog(title, content);
     }
