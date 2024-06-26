30ef8bd77eeafbb7311b47c6336110685081e967
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-06-26 23:24:08 +0200

    Update CommentRepository.java

--- src/main/java/ch/hftm/blogproject/control/CommentRepository.java
+++ src/main/java/ch/hftm/blogproject/control/CommentRepository.java
@@ -1,13 +1,17 @@
 package ch.hftm.blogproject.control;
 
+import java.util.List;
+
 import ch.hftm.blogproject.entity.Comment;
+
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import java.util.List;
+
+// This class serves as the interface between the CommentService class and the MySQL Database.
 
 @ApplicationScoped
 public class CommentRepository implements PanacheRepository<Comment> {
     public List<Comment> findByBlogId(Long blogId) {
-        return list("blog.id", blogId);
+        return find("blog.id", blogId).list();
     }
 }
