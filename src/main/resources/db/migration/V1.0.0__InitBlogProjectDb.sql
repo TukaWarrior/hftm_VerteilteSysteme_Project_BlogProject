8f9c246f6ca4922091fb4955e0b89e620ee156e9
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-16 18:33:07 +0200

    Implemented Flyway into application.properties for db migration.

---
+++ src/main/resources/db/migration/V1.0.0__InitBlogProjectDb.sql
@@ -0,0 +1,7 @@
+-- -- Entry Table
+-- CREATE TABLE Entry (
+--   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+--   content VARCHAR(255) DEFAULT NULL,
+--   likes INT NOT NULL,
+--   title VARCHAR(255) DEFAULT NULL
+-- );
