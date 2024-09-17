d8b81b3d0be9568e62a64c2962de8cc63398fc83
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-17 21:33:28 +0200

    Cleaning up DTO entities

--- src/main/java/ch/hftm/blogproject/model/dto/BlogPostDTO.java
+++ src/main/java/ch/hftm/blogproject/model/dto/BlogPostDTO.java
@@ -3,9 +3,6 @@ package ch.hftm.blogproject.model.dto;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import ch.hftm.blogproject.model.entity.BlogPost;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,10 +11,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-// @JsonIgnoreProperties(ignoreUnknown = true) // Ignore unknown fields during deserialization
-
 public class BlogPostDTO {
-    private Long blogPostID; // Present in GET responses, null for POST
+    private Long blogPostID;
     @NotBlank(message = "Title cannot be empty")
     private String title;
     @NotBlank(message = "Content cannot be empty")
@@ -26,19 +21,4 @@ public class BlogPostDTO {
     private ZonedDateTime createdAt;
     private ZonedDateTime lastChangedAt;
     private List<CommentDTO> comments;
-
-    // Convert from BlogPost entity to BlogPostDTO
-    // public BlogPostDTO(BlogPost blogPost) {
-    //     this.blogPostID = blogPost.getBlogPostID();
-    //     this.title = blogPost.getTitle();
-    //     this.content = blogPost.getContent();
-    //     this.createdAt = blogPost.getCreatedAt();
-    //     this.lastChangedAt = blogPost.getLastChangedAt();
-    //     this.accountId = blogPost.getAccountId();
-    // }
-
-    // Convert from DTO to BlogPost entity
-    // public BlogPost toEntity() {
-    //     return new BlogPost(this.title, this.content, this.accountId);
-    // }
 }
--- src/main/java/ch/hftm/blogproject/model/dto/CommentDTO.java
+++ src/main/java/ch/hftm/blogproject/model/dto/CommentDTO.java
@@ -2,41 +2,20 @@ package ch.hftm.blogproject.model.dto;
 
 import java.time.ZonedDateTime;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import ch.hftm.blogproject.model.entity.Comment;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true) // Ignore unknown fields during deserialization
 public class CommentDTO {
-    private Long commentID;  // Present in GET responses, null for POST
+    private Long commentID;
     private Long blogPostID;
     @NotBlank(message = "Content cannot be empty")
     private String content;
     private String creator;
-    private ZonedDateTime createdAt;  // Present in GET responses, not used in POST
-    private ZonedDateTime lastChangedAt;  // Present in GET responses, not used in POST
-
-    // Constructor for GET responses
-    // public CommentDTO(Comment comment) {
-    //     this.commentID = comment.getCommentID();
-    //     this.content = comment.getContent();
-    //     this.createdAt = comment.getCreatedAt();
-    //     this.lastChangedAt = comment.getLastChangedAt();
-    //     this.accountId = comment.getAccountId();
-    //     this.blogPostId = comment.getBlogPost().getBlogPostID();
-    // }
-
-    // Convert from DTO to entity
-    // public Comment toEntity() {
-    //     Comment comment = new Comment();
-    //     comment.setContent(this.content);
-    //     comment.setCreatedAt(ZonedDateTime.now());
-    //     comment.setLastChangedAt(ZonedDateTime.now());
-    //     return comment;
-    // }
+    private ZonedDateTime createdAt;
+    private ZonedDateTime lastChangedAt;
 }
