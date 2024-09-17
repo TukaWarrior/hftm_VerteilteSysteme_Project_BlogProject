47b948bc549e86c50204ba35d8d3cdd407423a51
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-17 21:37:32 +0200

    Cleaning up model classes.

--- src/main/java/ch/hftm/blogproject/model/entity/BlogPost.java
+++ src/main/java/ch/hftm/blogproject/model/entity/BlogPost.java
@@ -3,7 +3,14 @@ package ch.hftm.blogproject.model.entity;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,13 +36,4 @@ public class BlogPost {
     
     @OneToMany(mappedBy = "blogpost", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Comment> comments;
-
-    // Constructor for creating new blog posts
-    // public BlogPost(String title, String content, Long accountId) {
-    //     this.title = title;
-    //     this.content = content;
-    //     this.accountId = accountId;
-    //     this.createdAt = ZonedDateTime.now();
-    //     this.lastChangedAt = ZonedDateTime.now();
-    // }
 }
--- src/main/java/ch/hftm/blogproject/model/entity/Comment.java
+++ src/main/java/ch/hftm/blogproject/model/entity/Comment.java
@@ -1,7 +1,14 @@
 package ch.hftm.blogproject.model.entity;
 
 import java.time.ZonedDateTime;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,19 +20,15 @@ import lombok.NoArgsConstructor;
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long commentID;
     // @Column(nullable = false)
     private String content;
     private String creator;
     private ZonedDateTime createdAt;
     private ZonedDateTime lastChangedAt;
-    // private Long accountId;
 
     @ManyToOne
     @JoinColumn(name = "blogpost_id")
     private BlogPost blogPost;
-
-    // @ManyToOne
-    // @JoinColumn(name = "blogpost_id", nullable = false)
-    // private BlogPost blogPost;
 }
